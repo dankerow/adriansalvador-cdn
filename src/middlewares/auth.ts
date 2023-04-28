1d66ec876fb8cc7a9052fe3cd27b53d1280ef786
Dan <danker.twist@gmail.com>
2023-04-28 05:21:22 -0400

    feat: same authorization logic as the API auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,13 +1,21 @@
-export default async function(req, res, next) {
-	if (!req.headers.authorization) return res.status(401).send({ message: 'No authorization header provided' })
+import jwt from 'jsonwebtoken'
 
-	const token = req.headers.authorization.split(' ')[1]
-	if (!token) return res.status(401).send({ message: 'No authorization header provided' })
+export default function (req, res, next) {
+  if (!req.headers.authorization) return res.status(401).send({ message: 'No authorization header provided' })
 
-	const apiKeyVerification = token === process.env.AUTH_SECRET
-	if (apiKeyVerification) {
-		next()
-	} else {
-		return res.code(403).send({ error: { statusCode: 403, message: 'Invalid authorization.' } })
-	}
+  const token = req.headers.authorization.split(' ')[1]
+  if (!token) return res.status(401).send({ message: 'No authorization header provided' })
+
+  jwt.verify(token, process.env.AUTH_SECRET, async (error, decoded) => {
+    if (error) return res.status(401).send({ message: 'Invalid token' })
+
+    const id = decoded?.sub ?? null
+    if (!id) return res.status(401).send({ message: 'Invalid authorization' })
+
+    const user = await this.database.getUserById(id)
+    if (!user) return res.status(401).send({ message: 'User doesn\'t exist' })
+
+    req.user = user
+    next()
+  })
 }
