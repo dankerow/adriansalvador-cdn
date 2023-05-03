053cb8c0d6cb1fe2dd3ab1a44f2a8fcb9d99b4ae
Dan <danker.twist@gmail.com>
2023-05-02 23:59:44 -0700

    feat: choose number of workes by setting WORKERS_NUMBER en variable

--- src/cluster/primary.ts
+++ src/cluster/primary.ts
@@ -1,48 +1,51 @@
-import cluster, { Worker } from 'cluster';
-import os from 'os';
-import { Logger as logger } from '../utils';
+import 'dotenv/config'
 
-const workers: Map<any, any> = new Map();
-const workersLength: number = os.cpus().length;
+import type { Worker } from 'cluster'
+import cluster from 'cluster'
+import os from 'os'
+import { Logger as logger } from '../utils'
 
-logger.log('Primary', `Setting up ${workersLength} workers...`);
+const workers: Map<any, any> = new Map()
+const workersLength: number = (process.env.WORKERS_NUMBER ? parseInt(process.env.WORKERS_NUMBER) : false) || os.cpus().length
 
-cluster.setupPrimary();
+logger.log('Primary', `Setting up ${workersLength} workers...`)
+
+cluster.setupPrimary()
 
 for (let i = 0; i < workersLength; i++) {
-	const worker: Worker = cluster.fork();
-	workers.set(worker.id, worker);
+  const worker: Worker = cluster.fork()
+  workers.set(worker.id, worker)
 }
 
 cluster.on('message', (worker: Worker, message) => {
-	if (message.type) {
-		switch (message.type) {
-		case 'log':
-			logger.log(`Worker #${worker.id}`, message.content);
-			break;
-		case 'warn':
-			logger.warn(`Worker #${worker.id}`, message.content);
-			break;
-		case 'error':
-			logger.error(`Worker #${worker.id}`, message.content);
-			break;
-		}
-	}
-});
+  if (message.type) {
+    switch (message.type) {
+      case 'log':
+        logger.log(`Worker #${worker.id}`, message.content)
+        break
+      case 'warn':
+        logger.warn(`Worker #${worker.id}`, message.content)
+        break
+      case 'error':
+        logger.error(`Worker #${worker.id}`, message.content)
+        break
+    }
+  }
+})
 
 cluster.on('online', (worker: Worker) => {
-	logger.log('Primary', `Worker #${worker.id} is online`);
-});
+  logger.log('Primary', `Worker #${worker.id} is online`)
+})
 
 cluster.on('disconnect', (worker: Worker) => {
-	logger.warn('Primary', `Worker ${worker.id} disconnected`);
-});
+  logger.warn('Primary', `Worker ${worker.id} disconnected`)
+})
 
 cluster.on('exit', (worker: Worker, code, signal) => {
-	logger.log(`Worker #${worker.id}`, `Worker #${worker.id} died with code: ${code} and signal: ${signal}`);
-	logger.log('Primary', 'Starting a new worker');
+  logger.log(`Worker #${worker.id}`, `Worker #${worker.id} died with code: ${code} and signal: ${signal}`)
+  logger.log('Primary', 'Starting a new worker')
 
-	const newWorker: Worker = cluster.fork();
-	workers.delete(worker.id);
-	workers.set(newWorker.id, newWorker);
-});
+  const newWorker: Worker = cluster.fork()
+  workers.delete(worker.id)
+  workers.set(newWorker.id, newWorker)
+})
