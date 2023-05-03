7b452fc9c51780e3a49fb150b9b77bdd9812e35e
Dan <danker.twist@gmail.com>
2023-05-03 00:00:16 -0700

    feat: update galleryUploader script

--- scripts/galleryUploader.ts
+++ scripts/galleryUploader.ts
@@ -1,40 +1,20 @@
 import 'dotenv/config'
 
-import { readdir, readFile, stat } from 'fs/promises'
-import { extname, join } from 'path'
+import { readdir, readFile, stat } from 'node:fs/promises'
+import { join } from 'node:path'
+import crypto from 'node:crypto'
+import sharp from 'sharp'
 
 import { Database } from '../src/managers'
 
-import ExifReader from 'exifreader'
-import { v4 as uuidv4 } from 'uuid'
-import { imageHash } from 'image-hash'
-import sharp from 'sharp'
-
 const database = new Database()
-database.connect()
+await database.connect()
 
 const dir = join('src', 'static', 'gallery')
 
-const generateThumbnail = async (filename, directory) => {
-  const filePath = join(directory, filename)
-
-  const file = await readFile(filePath)
-  const ext = extname(filename).toLowerCase()
-  const output = `${filename.slice(0, -ext.length)}.webp`
-
-  await sharp(file)
-    .resize(64, 64)
-    .toFormat('webp')
-    .toFile(join('src', 'static', 'thumbs', 'square', output))
-  await sharp(file)
-    .resize(225)
-    .toFormat('webp')
-    .toFile(join('src', 'static', 'thumbs', output))
-}
-
 const loader = async (directory) => {
-  const folders = await readdir(directory);
-  console.log(directory)
+  const folders = await readdir(directory)
+  console.log('Folder', directory)
 
   if (folders.length > 0) {
     for (let i = 0; i < folders.length; i++) {
@@ -45,74 +25,70 @@ const loader = async (directory) => {
         await loader(join(directory, fileOrDirName))
       } else {
         const folderName = directory.slice((directory.lastIndexOf('\\') + 1) - directory.length)
-        let file = await database.findFileByName(fileOrDirName.toLowerCase())
 
+        let album = await database.findAlbumByName(folderName.toLowerCase())
+        if (!album) {
+          album = {
+            id: crypto.randomUUID(),
+            name: folderName,
+            coverId: null,
+            coverFallbackId: null,
+            draft: false,
+            nsfw: false,
+            hidden: false,
+            favorite: false,
+            featured: false,
+            postedAt: null,
+            createdAt: +new Date(),
+            modifiedAt: +new Date()
+          }
+
+          await database.insertAlbum(album)
+
+          console.log(`${folderName}: Album successfully created ✅`)
+        }
+
+        let file = await database.findFileByName(fileOrDirName.toLowerCase())
         if (!file) {
           file = {
-            id: uuidv4(),
+            id: crypto.randomUUID(),
             name: fileOrDirName,
             extname: fileOrDirName.slice(fileOrDirName.lastIndexOf('.') - fileOrDirName.length),
-            tags: null,
             type: null,
-            hash: null,
             size: null,
+            albumId: album.id,
             createdAt: +new Date(),
             modifiedAt: +new Date()
           }
 
           const fileBuffer = await readFile(join(directory, fileOrDirName))
-          file.tags = ExifReader.load(fileBuffer, { expanded: true })
-          file.type = file.tags.format?.value
+          const fileMetadata = await sharp(fileBuffer).metadata()
+          file.type = fileMetadata.format
+          file.height = fileMetadata.height
+          file.width = fileMetadata.width
           file.size = Buffer.byteLength(fileBuffer)
-
-          imageHash({
-            data: fileBuffer
-          }, 16, true, (error, data) => {
-            if(error) throw error;
-            file.hash = data
-          });
-
-          await generateThumbnail(fileOrDirName, directory)
 
           await database.insertFile(file)
 
-          // console.log(`${fileOrDirName}: Inserted in database ✅`)
-
-          let album = await database.findAlbumByName(folderName.toLowerCase())
-          if (!album) {
-            album = await database.insertAlbum({
-              id: uuidv4(),
-              name: folderName,
-              nsfw: false,
-              hidden: false,
-              createdAt: +new Date(),
-              modifiedAt: +new Date()
-            })
-          }
-
-          await database.updateFile(file.id, { albumId: album.id })
-          await database.updateAlbum(album.id, { modifiedAt: +new Date() })
-
-          // console.log(`${fileOrDirName}: Successfully updated ✅`)
+          console.log(`${fileOrDirName}: File successfully created ✅`)
         }
 
-        if (!file.albumId) {
-          let album = await database.findAlbumByName(folderName.toLowerCase())
-          if (!album) {
-            album = await database.insertAlbum({
-              id: uuidv4(),
-              name: folderName,
-              nsfw: false,
-              hidden: false,
-              createdAt: +new Date(),
-              modifiedAt: +new Date()
-            })
-          }
+        if (file.albumId !== album.id) {
+          await database.updateFile(file.id, {
+            albumId: album.id,
+            modifiedAt: +new Date()
+          })
 
-          await database.updateFile(file.id, { albumId: album.id })
-          await database.updateAlbum(album.id, { modifiedAt: +new Date() })
+          console.log(`${fileOrDirName}: File successfully updated ✅`)
+        }
 
-          console.log(`${fileOrDirName}: Successfully updated ✅`)
+        if (!album.coverFallbackId && i === 0) {
+          await database.updateAlbum(album.id, {
+            coverFallbackId: file.id,
+            modifiedAt: +new Date()
+          })
+
+          console.log(`${fileOrDirName}: Album successfully updated ✅`)
         }
       }
     }
