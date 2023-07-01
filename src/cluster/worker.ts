5a024adf8d533bdd83e335520425d81866cf63c0
Dan <danker.twist@gmail.com>
2023-06-30 21:34:13 -0700

    eat(cluster): add error handling to server setup in worker
    Implemented a dedicated function to encapsulate error handling logic. The server setup in worker is now carried out inside a try-catch block, ensuring errors during setup are properly handled and communicated. This change prevents server from abruptly stopping due to unhandled errors during its setup.

--- src/cluster/worker.ts
+++ src/cluster/worker.ts
@@ -1,8 +1,19 @@
-import 'dotenv/config'
 import { Server } from '../structures'
 
-const server = new Server()
-server.setup()
+const handleError = (error: Error) => {
+  if (process.send) {
+    process.send({ type: 'error', content: error.stack || error })
+  } else {
+    console.error(error)
+  }
+}
 
-process.on('uncaughtException', (error) => process.send({ type: 'error', content: error.stack || error }))
-process.on('unhandledRejection', (error: Error) => process.send({ type: 'error', content: error.stack || error }))
+const server = new Server()
+try {
+  server.setup()
+} catch (error) {
+  handleError(error)
+}
+
+process.on('uncaughtException', handleError)
+process.on('unhandledRejection', handleError)
