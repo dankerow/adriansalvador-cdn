f348d8e098b8fce7ffed97da52660fc12b6fb66b
Dan <danker.twist@gmail.com>
2023-06-30 21:33:00 -0700

    feat(cluster): Improve worker map typing and Logger import
    Refactored the worker map to use number and Worker in the Map generic, implying a mapping from worker ID numbers to Worker objects. This change improves the readability and type safety of the code. Furthermore, the logger import is simplified and standardised to use Logger directly which increases code consistency.

--- src/cluster/primary.ts
+++ src/cluster/primary.ts
@@ -3,12 +3,10 @@ import 'dotenv/config'
 import type { Worker } from 'node:cluster'
 import cluster from 'node:cluster'
 import os from 'node:os'
-import { Logger as logger } from '../utils'
+import { Logger } from '../utils'
 
-const workers: Map<any, any> = new Map()
+const workers: Map<number, Worker> = new Map()
 const workersLength: number = (process.env.WORKERS_NUMBER ? parseInt(process.env.WORKERS_NUMBER) : false) || os.cpus().length
-
-logger.log('Primary', `Setting up ${workersLength} workers...`)
 
 cluster.setupPrimary()
 
@@ -21,29 +19,29 @@ cluster.on('message', (worker: Worker, message) => {
   if (message.type) {
     switch (message.type) {
       case 'log':
-        logger.log(`Worker #${worker.id}`, message.content)
+        Logger.log(`Worker #${worker.id}`, message.content)
         break
       case 'warn':
-        logger.warn(`Worker #${worker.id}`, message.content)
+        Logger.warn(`Worker #${worker.id}`, message.content)
         break
       case 'error':
-        logger.error(`Worker #${worker.id}`, message.content)
+        Logger.error(`Worker #${worker.id}`, message.content)
         break
     }
   }
 })
 
 cluster.on('online', (worker: Worker) => {
-  logger.log('Primary', `Worker #${worker.id} is online`)
+  Logger.log('Primary', `Worker #${worker.id} is online`)
 })
 
 cluster.on('disconnect', (worker: Worker) => {
-  logger.warn('Primary', `Worker ${worker.id} disconnected`)
+  Logger.warn('Primary', `Worker ${worker.id} disconnected`)
 })
 
 cluster.on('exit', (worker: Worker, code, signal) => {
-  logger.log(`Worker #${worker.id}`, `Worker #${worker.id} died with code: ${code} and signal: ${signal}`)
-  logger.log('Primary', 'Starting a new worker')
+  Logger.log(`Worker #${worker.id}`, `Worker #${worker.id} died with code: ${code} and signal: ${signal}`)
+  Logger.log('Primary', 'Starting a new worker')
 
   const newWorker: Worker = cluster.fork()
   workers.delete(worker.id)
