f55e9fb861b9f80b61e6c917238ee6e17ca4372f
Dan <danker.twist@gmail.com>
2024-03-22 08:55:08 -0400

    chore: adjust logger class usage

--- src/cluster/primary.ts
+++ src/cluster/primary.ts
@@ -3,7 +3,9 @@ import 'dotenv/config'
 import type { Worker } from 'node:cluster'
 import cluster from 'node:cluster'
 import os from 'node:os'
-import { Logger } from '../utils'
+import { Logger } from '@/utils'
+
+const logger = new Logger()
 
 const workers: Map<number, Worker> = new Map()
 const workersLength: number = (process.env.WORKERS_NUMBER ? parseInt(process.env.WORKERS_NUMBER) : false) || os.cpus().length
@@ -19,29 +21,29 @@ cluster.on('message', (worker: Worker, message) => {
   if (message.type) {
     switch (message.type) {
       case 'log':
-        Logger.log(`Worker #${worker.id}`, message.content)
+        logger.log(`Worker #${worker.id}`, message.content)
         break
       case 'warn':
-        Logger.warn(`Worker #${worker.id}`, message.content)
+        logger.warn(`Worker #${worker.id}`, message.content)
         break
       case 'error':
-        Logger.error(`Worker #${worker.id}`, message.content)
+        logger.error(`Worker #${worker.id}`, message.content)
         break
     }
   }
 })
 
 cluster.on('online', (worker: Worker) => {
-  Logger.log('Primary', `Worker #${worker.id} is online`)
+  logger.log('Primary', `Worker #${worker.id} is online`)
 })
 
 cluster.on('disconnect', (worker: Worker) => {
-  Logger.warn('Primary', `Worker ${worker.id} disconnected`)
+  logger.warn('Primary', `Worker ${worker.id} disconnected`)
 })
 
 cluster.on('exit', (worker: Worker, code, signal) => {
-  Logger.log(`Worker #${worker.id}`, `Worker #${worker.id} died with code: ${code} and signal: ${signal}`)
-  Logger.log('Primary', 'Starting a new worker')
+  logger.log(`Worker #${worker.id}`, `Worker #${worker.id} died with code: ${code} and signal: ${signal}`)
+  logger.log('Primary', 'Starting a new worker')
 
   const newWorker: Worker = cluster.fork()
   workers.delete(worker.id)
